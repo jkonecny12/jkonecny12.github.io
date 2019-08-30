084d3412327454ccb55a0dae68828e95a4c7a5d2
Jiri Konecny <jkonecny@redhat.com>
2019-08-30 19:34:54 +0200

    Make changes to configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,13 +3,28 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Jiri Konecny'
 SITENAME = "Dragon's Lair"
-SITEURL = ''
+SITESUBTITLE = "With an age it comes the wisdom. Wise is the one who listens."
+SITEURL = 'http://blog.packetseekers.eu'
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Prague'
 
 DEFAULT_LANG = 'en'
+
+# Theme and theme config
+THEME = "./themes/Flex"
+SITELOGO = "/images/avatar.jpg"
+DISQUS_SITENAME = "packetseekers"
+MAIN_MENU = True
+USE_FOLDER_AS_CATEGORY = True
+MENUITEMS = (
+    ('Categories', '/categories.html'),
+    ('Tags', '/tags.html'),
+)
+PYGMENTS_STYLE = 'github'
+BROWSER_COLOR = '#333'
+# End theme and theme config
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,16 +34,19 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          # ('You can modify those links in your config file', '#'),
+#          )
+
+STATIC_PATHS = ['images']
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
