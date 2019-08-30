71574ae02e33643ce3a88d188ba2e83491f25b3a
Jiri Konecny <jkonecny@redhat.com>
2019-08-30 18:14:37 +0200

    Fix web address

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://packetseekers.eu'
+SITEURL = 'http://blog.packetseekers.eu'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
